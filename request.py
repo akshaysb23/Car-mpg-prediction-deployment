5e9839e601c0013e0e59260f096ac7744451b814
akshaysb23 <61949290+akshaysb23@users.noreply.github.com>
2020-08-08 21:06:06 +0530

    Delete request.py

--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'displacement':307, 'horsepower':130, 'weight':3504, 'origin_European':0, 'origin_Japanese':0, 'model year_76':0, 'model year_77':0, 'model year_78':0, 'model year_79':0, 'model year_80':0, 'model year_81':0, 'model year_82':0})
-
-print(r.json())
